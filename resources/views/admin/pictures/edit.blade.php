0963436c4905c8668cfd52eae5f5dbebde855b78
Tramaine Jackson <jackson.tramaine3@gmail.com>
2022-03-20 19:51:16 -0400

    Updated the pictures pages

--- resources/views/admin/pictures/edit.blade.php
+++ resources/views/admin/pictures/edit.blade.php
@@ -34,57 +34,65 @@
 				</div>
 			</div>
 
-			<div class="col-12" id="">
+			@if($getPictures->isNotEmpty())
 
-				<form name="" id="edit_picture_form" class="pictureForm" action="/pictures/{{ $trip->id }}" method="POST" enctype="multipart/form-data">
+				<div class="col-12" id="">
 
-					{{ method_field('PATCH') }}
-					{{ csrf_field() }}
+					<form name="" id="edit_picture_form" class="pictureForm" action="/pictures/{{ $trip->id }}" method="POST" enctype="multipart/form-data">
 
-					<div class="md-row">
-						<button type="submit" class="btn btn-secondary btn-lg ml-3">Update All</button>
+						{{ method_field('PATCH') }}
+						{{ csrf_field() }}
 
-						<div class="container-fluid" id="">
+						<div class="md-row">
+							<button type="submit" class="btn btn-secondary btn-lg ml-3">Update All</button>
 
-							<div class="row" id="">
+							<div class="container-fluid" id="">
 
-								@foreach($getPictures as $picture)
+								<div class="row" id="">
 
-									@php $content = Storage::disk('local')->has($picture->picture_name); @endphp
+									@foreach($getPictures as $picture)
 
-									<div class="col-12 col-md-6 col-lg-4 animated">
+										@php $content = Storage::disk('local')->has($picture->picture_name); @endphp
 
-										<div class="card my-2">
+										<div class="col-12 col-md-6 col-lg-4 animated">
 
-											<img src="{{ $content == true ? asset('storage/' . str_ireplace('public/', '', $picture->picture_name)) : '/images/skyline.jpg' }}" class="card-img-top" alt="{{ $picture->picture_caption }}" style="" />
+											<div class="card my-2">
 
-											<div class="card-body">
+												<img src="{{ $content == true ? asset('storage/' . str_ireplace('public/', '', $picture->picture_name)) : '/images/skyline.jpg' }}" class="card-img-top" alt="{{ $picture->picture_caption }}" style="" />
 
-												<div class="md-form" id="">
-													<input type="text" name="picture_caption[]" class="form-control" value="{{ $picture->picture_caption }}" placeholder="Enter Caption" />
-													<label class="card-title">Picture Caption</label>
+												<div class="card-body">
+
+													<div class="md-form" id="">
+														<input type="text" name="picture_caption[]" class="form-control" value="{{ $picture->picture_caption }}" placeholder="Enter Caption" />
+														<label class="card-title">Picture Caption</label>
+													</div>
+
+													<input type="text" name="picture_id[]" class="" value="{{ $picture->id }}" hidden />
 												</div>
 
-												<input type="text" name="picture_id[]" class="" value="{{ $picture->id }}" hidden />
-											</div>
-
-											<div class="card-footer">
-												<button class="btn btn-danger mx-auto d-block" onclick="event.preventDefault(); removePicture({{ $picture->id }});">Remove Picture</button>
+												<div class="card-footer">
+													<button class="btn btn-danger mx-auto d-block" onclick="event.preventDefault(); removePicture({{ $picture->id }});">Remove Picture</button>
+												</div>
 											</div>
 										</div>
-									</div>
 
-								@endforeach
+									@endforeach
+
+								</div>
 
 							</div>
 
 						</div>
 
-					</div>
+					</form>
 
-				</form>
+				</div>
 
-			</div>
+			@else
+				<div class="col-12 col-md-6 col-lg-4 py-4 animated">
+					<h2 class="h2">There are no picturs added for this trip yet. Click <a href="{{ route('pictures.create') }}">here</a> to add pictures.</h2>
+				</div>
+			@endif
 
 		</div>
 
