76e5dc8315052d9d7f86791462289f81461340a6
Tramaine Jackson <jackson.tramaine3@gmail.com>
2023-12-19 12:40:50 -0500

    Removed send a question section

--- resources/views/contact_us.blade.php
+++ resources/views/contact_us.blade.php
@@ -18,13 +18,13 @@
 						<div class="card-body">
 							<h4 class="card-title">Deborah Jackson</h4>
 							<p class="contact_email mb-1"><span class="font-weight-bold font-italic">Email:</span>&nbsp;<span><a href="mailto:jacksond1961@yahoo.com" class="">jacksond1961@yahoo.com</a></span></p>
-							<p class="contact_email"><span class="font-weight-bold font-italic">Phone:</span>&nbsp;<span>215-472-6036</span></p>
+							<p class="contact_email"><span class="font-weight-bold font-italic">Phone:</span>&nbsp;<span>215-472-6036 (EST)</span></p>
 						</div>
 
 						<div class="card-body">
 							<h4 class="card-title">Rhonda Lambert</h4>
 							<p class="contact_email mb-1"><span class="font-weight-bold font-italic">Email:</span>&nbsp;<span><a href="mailto:rhonda.lambert@sbcglobal.com" class="">rhonda.lambert@sbcglobal.com</a></span></p>
-							<p class="contact_email"><span class="font-weight-bold font-italic">Phone:</span>&nbsp;<span>707-208-7290</span></p>
+							<p class="contact_email"><span class="font-weight-bold font-italic">Phone:</span>&nbsp;<span>707-208-7290 (PST)</span></p>
 						</div>
 					</div>
 
@@ -32,66 +32,66 @@
 
 			</div>
 
-			<div class="row">
-				
-				<div class="col-10 col-sm-6 mx-auto my-4 rounded white ">
+{{--			<div class="row">--}}
+{{--				--}}
+{{--				<div class="col-10 col-sm-6 mx-auto my-4 rounded white ">--}}
 
-					<h2 class="text-center my-2">Send A Question</h2>
+{{--					<h2 class="text-center my-2">Send A Question</h2>--}}
 
-					<form id="question_form1" action="/questions" method="POST">
+{{--					<form id="question_form1" action="/questions" method="POST">--}}
 
-						{{ method_field('POST') }}
-						{{ csrf_field() }}
+{{--						{{ method_field('POST') }}--}}
+{{--						{{ csrf_field() }}--}}
 
-						<div class="md-form">
-							<input class="form-control" type="text" id="first_name" name="first_name" value="{{ old('first_name') }}" required  {{ $errors->first('email_address') ? 'first_name' : '' }}/>
+{{--						<div class="md-form">--}}
+{{--							<input class="form-control" type="text" id="first_name" name="first_name" value="{{ old('first_name') }}" required  {{ $errors->first('email_address') ? 'first_name' : '' }}/>--}}
 
-							@if($errors->has('first_name'))
-								<span class="text-danger">{{ $errors->first('first_name') }}</span>
-							@endif
+{{--							@if($errors->has('first_name'))--}}
+{{--								<span class="text-danger">{{ $errors->first('first_name') }}</span>--}}
+{{--							@endif--}}
 
-							<label for="first_name">First Name:</label>
-						</div>
-						
-						<div class="md-form">
-							<input class="form-control" type="text" id="last_name" name="last_name" value="{{ old('last_name') }}" required  {{ $errors->first('email_address') ? 'last_name' : '' }}/>
+{{--							<label for="first_name">First Name:</label>--}}
+{{--						</div>--}}
+{{--						--}}
+{{--						<div class="md-form">--}}
+{{--							<input class="form-control" type="text" id="last_name" name="last_name" value="{{ old('last_name') }}" required  {{ $errors->first('email_address') ? 'last_name' : '' }}/>--}}
 
-							@if($errors->has('last_name'))
-								<span class="text-danger">{{ $errors->first('last_name') }}</span>
-							@endif
+{{--							@if($errors->has('last_name'))--}}
+{{--								<span class="text-danger">{{ $errors->first('last_name') }}</span>--}}
+{{--							@endif--}}
 
-							<label for="last_name">Last Name:</label>
-						</div>
-						
-						<div class="md-form">
-							<input class="form-control" type="email" id="email_address" name="email_address" value="{{ old('email_address') }}" required  {{ $errors->first('email_address') ? 'autofocus' : '' }}/>
+{{--							<label for="last_name">Last Name:</label>--}}
+{{--						</div>--}}
+{{--						--}}
+{{--						<div class="md-form">--}}
+{{--							<input class="form-control" type="email" id="email_address" name="email_address" value="{{ old('email_address') }}" required  {{ $errors->first('email_address') ? 'autofocus' : '' }}/>--}}
 
-							@if($errors->has('email_address'))
-								<span class="text-danger">{{ $errors->first('email_address') }}</span>
-							@endif
+{{--							@if($errors->has('email_address'))--}}
+{{--								<span class="text-danger">{{ $errors->first('email_address') }}</span>--}}
+{{--							@endif--}}
 
-							<label for="email_address">Email Address:</label>
-						</div>
-						
-						<div class="md-form">
-							<textarea class="form-control md-textarea" id="question_text" name="question_text" rows="5" cols="15" required {{ $errors->first('question_text') ? 'autofocus' : '' }}>{{ old('question_text') }}</textarea>
+{{--							<label for="email_address">Email Address:</label>--}}
+{{--						</div>--}}
+{{--						--}}
+{{--						<div class="md-form">--}}
+{{--							<textarea class="form-control md-textarea" id="question_text" name="question_text" rows="5" cols="15" required {{ $errors->first('question_text') ? 'autofocus' : '' }}>{{ old('question_text') }}</textarea>--}}
 
-							@if($errors->has('question_text'))
-								<span class="text-danger">{{ $errors->first('question_text') }}</span>
-							@endif
+{{--							@if($errors->has('question_text'))--}}
+{{--								<span class="text-danger">{{ $errors->first('question_text') }}</span>--}}
+{{--							@endif--}}
 
-							<label for="question_text">Question:</label>
-						</div>
-						
-						<div class="md-form">
-							<input type="submit" id="submit_question" class="btn btn-info" onclick="sendQuestion();" value="Send" />
-						</div>
+{{--							<label for="question_text">Question:</label>--}}
+{{--						</div>--}}
+{{--						--}}
+{{--						<div class="md-form">--}}
+{{--							<input type="submit" id="submit_question" class="btn btn-info" onclick="sendQuestion();" value="Send" />--}}
+{{--						</div>--}}
 
-					</form>
+{{--					</form>--}}
 
-				</div>
+{{--				</div>--}}
 
-			</div>
+{{--			</div>--}}
 
 		</div>
 
